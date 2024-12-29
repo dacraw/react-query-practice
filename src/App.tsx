e63474f6629c46fd7dd18164cdfbae1cc6b9b21e
Doug <doug.a.crawford@gmail.com>
2024-12-28 20:47:03 -0800

    Fetch fake posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactQueryPlayground from "./components/ReactQueryPlayground";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <ReactQueryPlayground />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <ReactQueryPlayground />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/components/ReactQueryPlayground.tsx
+++ src/components/ReactQueryPlayground.tsx
@@ -1,7 +1,37 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+type Post = {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+};
+
+const getPosts = (): Promise<Post[]> => {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((data) =>
+    data.json()
+  );
+};
+
 export default function ReactQueryPlayground() {
+  const queryClient = useQueryClient();
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
+  if (isPending) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  console.log(data);
   return (
     <div>
-      <p className="text-blue-700">Empty</p>
+      <ul>
+        {data?.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
